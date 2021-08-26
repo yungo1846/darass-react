2f02b39bdac191edc4c4dca2a99434896d0647f7
yungo1846 <yungo1468@naver.com>
2021-08-26 13:53:59 +0900

    refactor: change function type to class

--- src/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-
-declare function Darass(projectKey: string): ReactNode;
-
-export default Darass;
---
+++ types/index.d.ts
@@ -0,0 +1,5 @@
+import React from "react";
+
+declare class Darass extends React.Component<{ projectKey: string }, {}> {}
+
+export default Darass;
