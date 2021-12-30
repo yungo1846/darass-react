5118fd74655521d44a6d4e3564463296a2e43b00
zereight <definedable@gmail.com>
2021-12-31 01:04:48 +0900

    refactor: update darass react

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,18 +1,18 @@
 const LOCALHOST_END_POINT = "https://localhost:3000";
-const DEVELOP_END_POINT = "https://deploy-script.darass.co.kr";
-const PRODUCTION_END_POINT = "https://deploy-script.darass.co.kr";
+const DEVELOP_END_POINT = "https://dev-reply-module.darass.co.kr";
+const PRODUCTION_END_POINT = "https://reply-module.darass.co.kr";
 
 const END_POINT_TABLE = {
   localhost: LOCALHOST_END_POINT,
   development: DEVELOP_END_POINT,
-  production: PRODUCTION_END_POINT
+  production: PRODUCTION_END_POINT,
 } as const;
 
 export const END_POINT = PRODUCTION_END_POINT;
 
 export const URL_REPLACE_TABLE = [
   { from: "://m.", to: "://" },
-  { from: "/m/", to: "/" }
+  { from: "/m/", to: "/" },
 ] as const;
 
 export const CLOSE_MODAL_ANIMATION_FINISH_TIME = 200;
